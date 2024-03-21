c226de9b43ed9aa7df631b5fffd11d1adde120e8
Piyush-Bhattarai <94219791+Piyush-Bhattarai@users.noreply.github.com>
2024-03-21 12:18:54 -0400

    Delete HospitalManagement directory

--- HospitalManagement/src/main/java/com/hospital/system/reprository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.system.reprository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hospital.system.model.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-
-}
--- HospitalManagement/src/main/java/com/hospital/system/reprository/SpecializationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.system.reprository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hospital.system.model.Specialization;
-
-public interface SpecializationRepository extends JpaRepository<Specialization, Integer>{
-
-}
