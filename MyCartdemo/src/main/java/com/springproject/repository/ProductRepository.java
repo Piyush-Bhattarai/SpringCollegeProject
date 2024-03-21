d0980bae6cf600b21442b6e86611e1eacc9b6ad3
Piyush-Bhattarai <94219791+Piyush-Bhattarai@users.noreply.github.com>
2024-03-21 12:19:03 -0400

    Delete MyCartdemo directory

--- MyCartdemo/src/main/java/com/springproject/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springproject.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-
-}
