39cd91ea135b05b845f17043d96fdf6b90c4d7b7
Piyush-Bhattarai <94219791+Piyush-Bhattarai@users.noreply.github.com>
2024-03-21 12:18:04 -0400

    Delete Communication_Project directory

--- Communication_Project/src/main/java/com/springproject/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springproject.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer>{
-
-}
